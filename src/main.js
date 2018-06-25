0060a9893baa1151e24d7918663934397587122b
Julien Mastromonaco <mastromonaco.julien@gmail.com>
2018-06-25 08:27:17 +0200

    Implement feature twitchEvent.getGlobalsEvents()
    Implement new feature to get globals (next) events from twitch profile.
    Add public method to get new events.
    Add mupltiple internal method to 'dispatch' tasks to many methods.
    
    Add _getEvents, it's real method that contain process to extract data (getGlobalEvents is a factory).
    Add _getPages, it's an internal method that used to create new page from puppeteer browser instance.
    Add _getDescription, it's used to extract description (string) from puppeteer page instance.
    Add _extractEvent, it's method used to extract events informations from page instance.
    Add _extractDescription, it's a method to extract description from event page.
    
    _extract.. methods are used to manage DOM. (puppeteer->page.evaluate()

--- src/main.js
+++ src/main.js
@@ -13,6 +13,113 @@ class TwitchEvent {
     }
     this.username = username
   }
+
+  /**
+   * Method used to extract globals events from twitch user.
+   * @param {boolean} [hasDescription=false] - The event description extraction state.
+   * @returns {Array<object>} Representing the globals events list.
+   */
+  getGlobalEvents (hasDescription = false) {
+    return this._getEvents(`https://www.twitch.tv/${this.username}/events`, null, hasDescription)
+  }
+
+  /**
+   * Internal private method used to extract needed events.
+   * @param {string} target - Representing the target url.
+   * @param {null|number} offset - Representing the number of events needed.
+   * @param {boolean} hasDescription - Representing the event description extraction state.
+   * @returns {Array<object>} Representing the events list.
+   */
+  async _getEvents (target, offset, hasDescription) {
+    let events
+
+    try {
+      const browser = await puppeteer.launch({
+        headless: true,
+        args: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox'
+        ]
+      })
+      const page = await this._getPage(browser, target)
+      events = await this._extractEvents(page, offset)
+      events = hasDescription ? await this._getDescriptions(browser, events) : events
+      await browser.close()
+    } catch (error) {
+      console.log(error)
+    }
+    return events
+  }
+  
+  /**
+   * Internal private async method used to create new page and goto targeted url.
+   * @param {Browser} browser - Representing the puppeteer browser instance.
+   * @param {string} target - Representing the target url.
+   * @returns {Page} Representing new page instance created by browser (puppeteer).
+   */
+  async _getPage (browser, target) {
+    const page = await browser.newPage()
+    await page.goto(target, { waitUntil: 'networkidle2' })
+    return page
+  }
+
+  /**
+   * Internal private async method used to assign description for each event.
+   * @param {Browser} browser - Representing the puppeteer browser instance.
+   * @param {Array<object>} events - Representing the events list.
+   * @returns {Array<object>} Representing the updated events list.
+   */
+  async _getDescriptions (browser, events) {
+    for (let i = 0; i < events.length; ++i) {
+      const page = await this._getPage(browser, events[i].link)
+      events[i].description = await this._extractDescription(page)
+      await page.close()
+    }
+    return events
+  }
+
+  /**
+   * Internal private method used to extract events from "virtual page" (DOM scrapping).
+   * @param {Page} page - Representing puppeteer page instance.
+   * @param {null|number} offset - Representing the number of events needed.
+   * @returns {Promise<object>} - Representing events extract from events page.
+   */
+  _extractEvents (page, offset) {
+    return page.evaluate((offset) => {
+      let formatedEvents = []
+      const visibleEvents = document.querySelectorAll('.tw-c-background.tw-elevation-1 .tw-mg-x-2')
+      const selectedEvents = Array.from(visibleEvents).splice(0, offset || visibleEvents.length)
+      const innerSelector = (target, selector) => target.querySelector(selector).innerText || null
+
+      // TODO: Load more events. Need to simulate a virtual scrolldown.
+
+      selectedEvents.forEach((selectedEvent) => {
+        const [streamer, game] = innerSelector(selectedEvent, ':nth-child(3n)').split(/ streaming /)
+        formatedEvents.push({
+          link: selectedEvent.querySelector(':nth-child(1n)').href,
+          title: innerSelector(selectedEvent, ':nth-child(1n) h4'),
+          date: innerSelector(selectedEvent, ':nth-child(2n)'),
+          game,
+          streamer
+        })
+      })
+      return formatedEvents
+    }, offset)
+  }
+
+  /**
+   * Internal private method used to extract description from linked event page (DOM scrapping).
+   * @param {Page} page - Representing puppeteer page instance.
+   * @returns {Promise<object>} Representing the event description.
+   */
+  _extractDescription (page) {
+    return page.evaluate(() => {
+      const description = document.querySelector('.events-landing-collection__main-col')
+        ? document.querySelector('.events-landing-collection__main-col .tw-elevation-2 p span').innerText
+        : document.querySelector('.simplebar-scroll-content .tw-flex-grow-1 p span').innerText
+      return description || null
+    })
+  }
 }
 
 module.exports = TwitchEvent
