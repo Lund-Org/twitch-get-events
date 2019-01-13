f15875f04b9e9daea630e3d7fe694e4730a5dd8d
Julien Mastromonaco <mastromonaco.julien@gmail.com>
2019-01-13 16:35:24 +0100

    fix: Fix 'src/utils.js' method 'prepareArrayWithOffset()'

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 function prepareArrayWithOffset (offset, standard) {
   return Array(Math.floor(offset / standard))
     .fill(standard)
-    .concat(offset > 100 ? [offset % standard] : [])
+    .concat(offset > standard ? [offset % standard] : [offset])
 }
 
 module.exports = {
