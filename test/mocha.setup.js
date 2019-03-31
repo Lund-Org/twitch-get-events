5ebd02b138be597a8aaa6de83ae2033011959f0e
Julien Mastromonaco <mastromonaco.julien@gmail.com>
2019-03-31 01:50:37 +0100

    fix: Add value.trim() to pseudo .env parser to resolve whitespace mistakes

--- test/mocha.setup.js
+++ test/mocha.setup.js
@@ -8,12 +8,14 @@ if (!process.env.CI) {
       .split('\n')
       .map((line) => line.split('='))
       .forEach(([name, value]) => Object.assign(process.env, {
-        [name]: value
+        [name.trim()]: value.trim()
       }))
   } catch (err) {
     console.error('Can not load .env file.')
   }
 }
+
+console.log({ name: process.env.TWITCH_USERNAME })
 
 process.env.NODE_ENV = 'test'
 global.__root = path.resolve(__dirname, '../')
